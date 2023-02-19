0945b358fed3c407289680336a134b4a19ded816
Salih Cakmak <slhckmk01@gmail.com>
2023-02-19 22:19:03 +0300

    TableView Delegate and Data Source Complate

---
+++ RealmExample/TodoTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TodoTableViewController.swift
+//  RealmExample
+//
+//  Created by Salih on 19.02.2023.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+    
+    let itemArray = ["ADANA","MERSİN","ANKARA"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+   
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    // MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+
+        }
+    }
+        
+    
+}
