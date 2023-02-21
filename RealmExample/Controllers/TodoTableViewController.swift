17d314954a3bb90c634a4a463c691210fa9e20ec
Salih Cakmak <slhckmk01@gmail.com>
2023-02-22 00:39:06 +0300

    Convert to MVC Design pattern

---
+++ RealmExample/Controllers/TodoTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  TodoTableViewController.swift
+//  RealmExample
+//
+//  Created by Salih on 19.02.2023.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+    
+    var itemArray:[Item] = []
+    let userDefaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        newItem()
+        if let items = userDefaults.array(forKey: "list") as? [Item] {
+            itemArray = items
+        }
+        
+    }
+    
+    // MARK: - Function
+    
+    func makeAlert(title:String,message:String){
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Ok", style: .default)
+        
+        let action = UIAlertAction(title: "Add", style: .default) {(action) in
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            self.userDefaults.set(self.itemArray, forKey: "list")
+            self.tableView.reloadData()
+            
+        }
+        alert.addAction(action)
+        
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Yeni şehir ekle"
+            textField = alertTextField
+        }
+        self.present(alert, animated: true)
+    }
+    
+    func newItem(){
+        
+        let newItem = Item()
+        newItem.title = "Adana"
+        newItem.checkMark = false
+        itemArray.append(newItem)
+        
+    }
+    
+
+
+    // MARK: - Table view data source
+
+   
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let item = itemArray[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.checkMark == true ? .checkmark : .none
+        return cell
+        
+    }
+    
+    // MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].checkMark = !itemArray[indexPath.row].checkMark
+        
+        tableView.reloadData()
+        print("didSelect")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+  
+    
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        makeAlert(title: "Şehir", message: "Yeni Şehir Ekle")
+        
+    }
+    
+    
+    
+}
---
+++ RealmExample/DataModel/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  RealmExample
+//
+//  Created by Salih on 21.02.2023.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title:String = ""
+    var checkMark:Bool = true
+  
+}
